a0722aa68c5ac7e024df4227d6449a279dc951af
Daniel Duton <daniel@duton.lu>
2019-08-29 15:15:24 +0200

    rounded stroke

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -21,6 +21,13 @@
 import paper from 'paper'
 
 export default {
+    props: {
+        strokeWidth: {
+            type: Number,
+            default: 40
+        }
+    },
+
     data () {
         return {
             path: '',
@@ -51,11 +58,18 @@ export default {
         onMouseDown (evt, isTouch) {
             let x = isTouch ? evt.touches[0].clientX : evt.clientX
             let y = isTouch ? evt.touches[0].clientY : evt.clientY
+            let targetPoint = paper.view.viewToProject(new paper.Point(x, y))
+
+            this.startPath = new paper.Path.Circle(targetPoint, this.strokeWidth / 2)
+            this.startPath.fillColor = '#F00'
+            this.hasMoved = false
 
             this.path = new paper.Path()
             this.path.strokeColor = '#F00'
-            this.path.strokeWidth = 3
-            this.path.moveTo(paper.view.viewToProject(new paper.Point(x, y)))
+            this.path.strokeWidth = this.strokeWidth
+            this.path.strokeCap = 'round'
+            this.path.strokeJoin = 'round'
+            this.path.moveTo(targetPoint)
 
             if (isTouch) {
                 this.$el.addEventListener('touchmove', this.onTouchMove)
@@ -67,11 +81,19 @@ export default {
         onMouseMove (evt, isTouch) {
             let x = isTouch ? evt.touches[0].clientX : evt.clientX
             let y = isTouch ? evt.touches[0].clientY : evt.clientY
+            let targetPoint = paper.view.viewToProject(new paper.Point(x, y))
 
-            this.path.lineTo(paper.view.viewToProject(new paper.Point(x, y)))
+            this.hasMoved = true
+            this.startPath.remove()
+
+            this.path.lineTo(targetPoint)
         },
 
         onMouseUp (evt, isTouch) {
+            if (!this.hasMoved) {
+                this.startPath.fillColor = '#000'
+            }
+
             this.path.strokeColor = '#000'
             this.path.simplify()
 
